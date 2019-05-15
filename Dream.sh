d08ea42ca8b7a54f30d3d738007620f1d18313fc
DICKYalone <50066561+DICKYalone@users.noreply.github.com>
2019-05-15 18:59:45 +0700

    Rename Dickygt.sh to Dream.sh
    Tools sepesial 1k subscriber

---
+++ Dream.sh
@@ -0,0 +1,156 @@
+#usr/bin/bash
+clear
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Recode ajh mmnx
+echo
+echo
+#ini paket untuk menjalankan tools
+dicky.sh
+echo
+clear
+echo
+echo $me"                     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶"
+echo $me"                 ¶¶¶¶¶¶             ¶¶¶¶¶¶¶"
+echo $me"              ¶¶¶¶                        ¶¶¶¶"
+echo $me"             ¶¶¶                            ¶¶¶"
+echo $me"            ¶¶                                ¶¶"
+echo $me"           ¶¶                                  ¶¶"
+echo $me"          ¶¶                                   ¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"         ¶¶  ¶¶                            ¶¶´¶¶"
+echo $me"          ¶¶´´¶¶                           ¶¶´´¶¶"
+echo $me"           ¶¶´¶¶   ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶   ¶¶´¶¶"
+echo $me"            ¶¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶"
+echo $me"             ¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶"
+echo $me"    ¶¶¶       ¶¶  ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶¶"
+echo $me"   ¶¶¶¶¶     ¶¶   ¶¶¶¶¶¶¶   ¶¶¶   ¶¶¶¶¶¶¶   ¶¶     ¶¶¶¶¶¶"
+echo $me"  ¶¶   ¶¶    ¶¶     ¶¶¶    ¶¶¶¶¶    ¶¶¶     ¶¶    ¶¶   ¶¶"
+echo $me" ¶¶¶    ¶¶¶¶  ¶¶          ¶¶¶¶¶¶¶          ¶¶  ¶¶¶¶    ¶¶¶"
+echo $me"¶¶         ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶        ¶¶¶¶¶¶¶¶¶        ¶¶"
+echo $me"¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶      ¶¶¶¶¶¶¶¶"
+echo $me"  ¶¶¶¶ ¶¶¶¶¶      ¶¶¶¶¶              ¶¶¶ ¶¶     ¶¶¶¶¶¶ ¶¶¶"
+echo $me"       ¶¶¶¶¶¶  ¶¶¶  ¶¶           ¶¶  ¶¶¶  ¶¶¶¶¶¶"        
+echo $me"              ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶"
+echo $me"                  ¶¶ ¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶¶"
+echo $me"                ¶¶¶¶  ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶   ¶¶¶¶¶"
+echo $me"            ¶¶¶¶¶ ¶¶   ¶¶¶¶¶¶¶¶¶¶¶¶¶   ¶¶ ¶¶¶¶¶"
+echo $me"    ¶¶¶¶¶¶¶¶¶¶     ¶¶                 ¶¶      ¶¶¶¶¶¶¶¶¶"
+echo $me"   ¶¶           ¶¶¶¶¶¶¶             ¶¶¶¶¶¶¶¶          ¶¶"
+echo $me"    ¶¶¶     ¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶     ¶¶¶"
+echo $me"      ¶¶   ¶¶¶           ¶¶¶¶¶¶¶¶¶           ¶¶¶   ¶¶"
+echo $me"      ¶¶  ¶¶                                   ¶¶  ¶¶"
+echo $me"       ¶¶¶¶                                     ¶¶¶¶"
+echo
+echo
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo $i  "AUTHOR   : DICKY FEBRIAN"
+echo $i  "WA       : 085707706430"
+echo $i  "YOUTUBE  : DICKY GT"
+echo $i  "TOOLS    : INSTALLER (7 TOOLS)"
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo
+echo
+echo "###DAFTAR TOOLS###"
+echo 
+echo $cy "[1]"$bi" Brute Force Facebook"
+echo "=================================================="
+echo $cy "[2]"$i" Banguni Sahur"
+echo "=================================================="
+echo $cy "[3]"$me" Skull Project"
+echo "=================================================="
+echo $cy "[4]"$ku" Termux Alice (Khusus Jones)"
+echo "=================================================="
+echo $cy "[5]"$pur" Mata mata"
+echo "=================================================="
+echo $cy "[6]"$pu" Trap Project"
+echo "=================================================="
+echo $cy "[7]"$cy" Sadap Camera"
+echo "=================================================="
+echo $cy "[8]"$me" EXIT"
+echo "=================================================="
+echo
+echo
+echo $cy"┌==="$bi"[ DICKY GT ]"
+echo $cy"¦"
+read -p">>>" pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+badh new.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+bash troll.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/zlucifer/skull_project
+cd  skull_project
+bash skull.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/zlucifer/termux_alice
+cd termux_alice
+fi
+
+if [ $pil = 5 ]
+then
+clear
+git clone https://github.com/zlucifer/paladin_project
+cd paladin_project
+bash,paladin.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+git clone https://github.com/zlucifer/trap_project
+cd trap_project
+bash trap.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Bug  Bisa Nanya Kepada Saya"
+sleep 2
+echo $i"WhatsApp :"$i" 085707706430"
+echo $bi"Facebook :"$i" Dicky Febrian"
+exit
+fi
